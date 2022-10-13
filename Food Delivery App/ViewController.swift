eff5609d5e8443feb77af65656c6f8ebb18922d0
Dmitry <radiator211@mail.ru>
2022-10-13 23:57:29 +0700

    Initial Commit

---
+++ Food Delivery App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Food Delivery App
+//
+//  Created by Дмитрий Бессонов on 13.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
